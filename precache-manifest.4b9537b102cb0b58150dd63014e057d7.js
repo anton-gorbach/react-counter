0d70e5e3eed3ac2be5d4746f4e4d97dfe5700623
Anton Gorbach <anton.s.gorbach@gmail.com>
2020-03-10 02:30:43 +0200

    Updates

---
+++ precache-manifest.4b9537b102cb0b58150dd63014e057d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c56144c3dfdb9bbb2441dad7313e2b62",
+    "url": "/react-counter/index.html"
+  },
+  {
+    "revision": "0ae204101dc0863fb078",
+    "url": "/react-counter/static/css/main.ae908b26.chunk.css"
+  },
+  {
+    "revision": "f271997ad6a8754c9de3",
+    "url": "/react-counter/static/js/2.2d0d090f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-counter/static/js/2.2d0d090f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ae204101dc0863fb078",
+    "url": "/react-counter/static/js/main.4148ab9d.chunk.js"
+  },
+  {
+    "revision": "f19dd3762d2ae97e552e",
+    "url": "/react-counter/static/js/runtime-main.2f179ba8.js"
+  }
+]);
